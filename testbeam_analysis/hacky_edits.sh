2d84b17cfddcf63d2ede5568dd20ea09b7eaad1b
Edi <ploedi313@gmail.com>
2024-07-28 23:47:29 +0200

    Threshold scans: hacky_edits script to make alterations to plotting scripts.

---
+++ testbeam_analysis/hacky_edits.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+# set -x
+
+# Script to edit the plotting scripts for threshold scans in ITS3utils without requiring a submodule update
+
+
+
+
+## We start by checking for the ITS3utils dir
+its3_utils_path=`find . -type d -name "ITS3utils"`
+if [ -n "$its3_utils_path" ]; then
+    its3_utils_path=`realpath $its3_utils_path`
+else 
+    echo "Cannot find dir \"ITS3utils\". Please make sure it is visible from within \"`pwd`\"."
+    exit
+fi
+
+# Edit corry/plot_util.py to print efficiencies
+res_captured=`sed -n '609p' ${its3_utils_path}/corry/plot_util.py`
+if [ "${res_captured}" == "    if(optGaus):" ]; then
+    echo -e "Seems like file \"plot_util.py\" has already been edited."
+else
+    sed -i '609d' ${its3_utils_path}/corry/plot_util.py
+    sed -i '608a\
+\ \ \ \ if(optGaus):\
+\ \ \ \ \ \ tmp_gaus = self.optimise_hist_gaus(htmp, scale)\
+\ \ \ \ \ \ print(tmp_gaus)' ${its3_utils_path}/corry/plot_util.py
+fi
+
+# Edit corry/plot_analog_ce65v2.py to print efficiencies
+eff_captured=`sed -n '458p' ${its3_utils_path}/corry/plot_analog_ce65v2.py`
+if [ "${eff_captured}" == "  # Printing efficiencies for computing efficiency vs threshold plots" ]; then
+    echo -e "Seems like file \"plot_analog_ce65v2.py\" has already been edited."
+else
+    sed -i '456a\
+\
+\ \ # Printing efficiencies for computing efficiency vs threshold plots\
+\ \ print(\"Efficiencies\")\
+\ \ print(str(eff*100)+\" \"+str(uerr*100)+\" \"+str(lerr*100))\
+' ${its3_utils_path}/corry/plot_analog_ce65v2.py
+fi
+
